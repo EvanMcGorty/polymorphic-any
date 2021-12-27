31f11e6b6c5b86262fa5ac29528126554f0fa809
Evan McGorty <evanjamesmcgorty@gmail.com>
2021-12-27 17:42:03 -0500

    make virt<any> possible

--- virt.h
+++ virt.h
@@ -8,20 +8,20 @@
 //make everything constexpr
 //custom stack based alloc function for unique_ptr, have a byte[] buffer in virt that unique_ptr points to when the target object is small enough
 //virt second template argument that takes a size_t wrapper or a byte[] wrapper that automatically calculates the largest size from a list of types
-//but be sure to be able to distinguish between a pure variant and one that is also an any
+//but be sure to be able to distinguish between a pure variant and one that is also an virt
 //second template parameter that includes pointer type should be the same thing that can transform this into a variant
 // 
 //need to cover:
-//any unconstrained, any constrained to base class
-//		a generic constraint (compile time) can also be given on the classes that may be used with any
-//any with unique_ptr, any with shared_ptr, etc
-//any with a minimum list of types that it may contain, any without this list (same as the list being empty)
-//any with stack based memory buffer of at least N, any without buffer (same as the buffer being N=0)
-//if any has a buffer:
+//virt unconstrained, virt constrained to base class
+//		a generic constraint (compile time) can also be given on the classes that may be used with virt
+//virt with unique_ptr, virt with shared_ptr, etc
+//virt with a minimum list of types that it may contain, virt without this list (same as the list being empty)
+//virt with stack based memory buffer of at least N, virt without buffer (same as the buffer being N=0)
+//if virt has a buffer:
 //		buffer can be set to be at least big enough to acommodate the list of the types which it may contain
 //		or not
-//if any has a type list
-//		any can disallow objects whose types are not in this list (in this case the manually set buffer is meaningless)
+//if virt has a type list
+//		virt can disallow objects whose types are not in this list (in this case the manually set buffer is meaningless)
 //		or can disallow objects whose types are not on this list unless they fit in the buffer
 //		or can simply allow all objects to at least be allocated on the free store if needed
 //can choose which operations are expected of its objects (copy construction
@@ -46,11 +46,11 @@ to_t my_dynamic_cast(from_t* a)
 	return dynamic_cast<to_t>(a);
 }
 
-template<typename to_t, typename from_t>
+template<typename to_t, typename from_t, typename error_string_t = char const* >
 	requires std::is_pointer_v<to_t>
-to_t my_static_cast(from_t* a)
+to_t my_static_cast(from_t* a, error_string_t&& error = "static_cast fail")
 {
-	my_assert([&]{return my_dynamic_cast<to_t>(a)!=nullptr;},"static_cast fail");
+	my_assert([&]{return my_dynamic_cast<to_t>(a)!=nullptr;},std::forward<error_string_t>(error));
 	return static_cast<to_t>(a);
 }
 
@@ -64,7 +64,7 @@ std::unique_ptr<to_t> uptrcast(std::unique_ptr<from_t>&& a)
 }
 
 
-class universal_base
+class any
 {
 private:
 
@@ -74,8 +74,8 @@ template<typename t>
 friend class virt;
 
 	//generic operations without consistent return types
-	virtual std::unique_ptr<universal_base> copy_construct() const = 0;
-	virtual std::unique_ptr<universal_base> move_construct() && = 0;
+	virtual std::unique_ptr<any> copy_construct() const = 0;
+	virtual std::unique_ptr<any> move_construct() && = 0;
 	//make reference/pointer
 	//make const reference/pointer
 	//dereference
@@ -83,7 +83,7 @@ friend class virt;
 	//make optional?
 	//make std::unique_ptr?
 	//make arbitrary templated class/call arbitrary templated function given by template parameter in virt
-	//		maybe make almost_universal_base<t> derive from universal base in cases when this template parameter is supplied
+	//		maybe make almost_any<t> derive from any in cases when this template parameter is supplied
 	//call arbitrary template<t> function by passing in an id so derived wrapper knows which templated function to call
 	//		derived_wrapper will have a list of callables in its template arguments
 	//		maybe the id could be a unique type_id* like object of a wrapper of the callable template
@@ -96,7 +96,7 @@ friend class virt;
 	//ostream << operator
 	//istream >> operator
 	//[size_t] index operator
-	//conversion to some_iterator<ptr<universal_base>> (by iterating through underlying type if possible)
+	//conversion to some_iterator<ptr<any>> (by iterating through underlying type if possible)
 	//operator bool()
 	//conversions to common types (int, double, string)
 	//operations with multiple generic arguments
@@ -105,17 +105,17 @@ friend class virt;
 
 	//virtual size_t sizeof_most_derived() const = 0;
 	//typeid of most derived object
-	//return std::function<ptr<universal_base>(ptr<universal_base>)>(target)
+	//return std::function<ptr<any>(ptr<any>)>(target)
 
 	//maybe not needed:
-	//virtual universal_base* universal_base_this() const = 0;
+	//virtual any* any_this() const = 0;
 	//virtual void* derived_wrapper_this() const = 0;
 
 };
 
 
 template<typename t>
-class derived_wrapper final : public t, public universal_base //declared final so that sizeof can be determined for the whole object
+class derived_wrapper final : public t, public any //declared final so that sizeof can be determined for the whole object
 {
 public:
 
@@ -125,27 +125,45 @@ public:
 	template<typename...args>
 	derived_wrapper(args&&...vals) : t{ std::forward<args>(vals)... } {}
 
-	std::unique_ptr<universal_base> copy_construct() const override final
+	std::unique_ptr<any> copy_construct() const override final
 	{
-		return { std::make_unique<derived_wrapper>(*my_static_cast<t const*>(this)) };
+		if constexpr (std::is_copy_constructible_v<t>)
+		{
+			return { std::make_unique<derived_wrapper>(*my_static_cast<t const*>(this)) };
+		}
+		else
+		{
+			return {nullptr};
+		}
 	}
 
-	std::unique_ptr<universal_base> move_construct() && override final
+	std::unique_ptr<any> move_construct() && override final
 	{
-		return { std::make_unique<derived_wrapper>(std::move(*my_static_cast<t*>(this))) };
+		if constexpr (std::is_move_constructible_v<t>)
+		{
+			return { std::make_unique<derived_wrapper>(std::move(*my_static_cast<t*>(this))) };
+		}
+		else
+		{
+			return { nullptr };
+		}
 	}
+
 };
 
+
 template<typename from_t, typename error_string_t = char const*>
-universal_base* cast_to_universal_base(from_t* from, error_string_t&& error = "bad universal_base cast")
+any* my_static_cast(from_t* from, error_string_t&& error = "bad static cast to any*")
 {
-	universal_base* result = static_cast<universal_base*>(static_cast<derived_wrapper<from_t>*>(from));
+	any* result = static_cast<any*>(static_cast<derived_wrapper<from_t>*>(from));
 	//technically unsafe because there is no guarantee that from_t actually is also a derived_wrapper<from_t>,
 	//but theoretically this should always work
-	my_assert([&] {return my_dynamic_cast<universal_base*>(from) == result; }, std::forward<error_string_t>(error));
+	my_assert([&] {return my_dynamic_cast<any*>(from) == result; }, std::forward<error_string_t>(error));
 	return result;
 }
 
+template<typename derived_t, typename base_t>
+concept special_derived_from = std::derived_from<derived_t,base_t> || std::same_as<base_t,any>;
 
 template<typename t>
 class virt_data
@@ -195,8 +213,8 @@ public:
 	void give_unique_ptr(std::unique_ptr<t>&& a)
 	{
 		my_assert([&] {return bool{ a }; }, "virt_data should not be given a nullptr");
-		cast_to_universal_base(&*a, "virt_data must be given a pointer to type derived_wrapper<t>");
-		//cast to universal base has an assert in it
+		my_dynamic_cast<any*>(&*a, "virt_data must be given a pointer to type derived_wrapper<t>");
+		//cast to any has an assert in it
 		ptr = std::move(a);
 	}
 
@@ -213,7 +231,7 @@ public:
 		return my_dynamic_cast<tar_t*>(&*ptr) != nullptr;
 	}
 
-	template<std::derived_from<t> tar_t>
+	template<special_derived_from<t> tar_t>
 	virt_data<tar_t> downcast() &&
 	{
 		my_assert([&] {return bool{ ptr }; }, "cannot downcast a nullptr");
@@ -254,7 +272,6 @@ struct underlying_type_wrapper<t>
 	using type = t;
 };
 
-
 //nullptr state can be achieved by default initialization, or by move constructing from ::data().
 //This is an invalid state for any operation other than dereferencing
 template<typename raw_t>
@@ -264,6 +281,7 @@ private:
 
 	using stored_t = typename underlying_type_wrapper<raw_t>::type; //if raw_t is polymorphic, stored_t is the same, otherwise, stored_t is wrapper of raw_t that makes it polymorphic
 
+
 public:
 
 	virt_data<stored_t> data;
@@ -271,9 +289,9 @@ public:
 	//template<typename result_t, typename...ts>
 	//friend virt<result_t> make_virt(ts&&...vals);
 
-	explicit virt() : data{} {}
+	explicit virt() : data{} {} //data{} initializes to nullptr
 
-	//template<std::derived_from<raw_t> rhs_t>
+	//template<special_derived_from<raw_t> rhs_t>
 	//virt(virt_data<rhs_t>&& a)
 	//{
 	//	my_assert([&] {return bool{ a.ptr }; }, "virt may not be assigned an empty (nullptr) virt_data");
@@ -281,7 +299,7 @@ public:
 	//}
 
 
-	////template<std::derived_from<raw_t> rhs_t>
+	////template<special_derived_from<raw_t> rhs_t>
 	//template<typename rhs_t>
 	//virt& operator=(virt_data<rhs_t>&& a)
 	//{
@@ -291,72 +309,84 @@ public:
 	//}
 
 
-	template<std::derived_from<raw_t> rhs_t>
+	template<special_derived_from<raw_t> rhs_t>
 	virt(virt<rhs_t>&& a)
 	{
-		my_assert([&] {return a.data.ptr != nullptr; }, "virt may not be constructed from an empty (nullptr) virt");
-		//my_assert([&] {return my_dynamic_cast<universal_base*>(&*a.data.ptr) != nullptr; },
-		//	"virt pointer must point to type universal_base");
-		universal_base* target = cast_to_universal_base(&*a.data.ptr);
-		data.ptr = uptrcast<stored_t>(std::move(*target).move_construct());
+		if (a.data.ptr != nullptr)
+		{
+			any* target = my_dynamic_cast<any*>(&*a.data.ptr);
+			data.ptr = uptrcast<stored_t>(std::move(*target).move_construct());
+		}
+		else
+		{
+			data.ptr = nullptr;
+		}
 	}
 
 	virt(virt&& a)
 	{
-		my_assert([&] {return a.data.ptr != nullptr; }, "virt may not be constructed from an empty (nullptr) virt");
-		//my_assert([&] {return my_dynamic_cast<universal_base*>(&*a.data.ptr) != nullptr; },
-		//	"virt pointer must point to type universal_base");
-		universal_base* target = cast_to_universal_base(&*a.data.ptr);
-		data.ptr = uptrcast<stored_t>(std::move(*target).move_construct());
+		if (a.data.ptr != nullptr)
+		{
+			any* target = my_dynamic_cast<any*>(&*a.data.ptr);
+			data.ptr = uptrcast<stored_t>(std::move(*target).move_construct());
+		}
+		else
+		{
+			data.ptr = nullptr;
+		}
 	}
 
 
-	template<std::derived_from<raw_t> rhs_t>
+	template<special_derived_from<raw_t> rhs_t>
 	virt(virt<rhs_t> const& a)
 	{
-		my_assert([&] {return a.data.ptr != nullptr; }, "virt may not be constructed from an empty (nullptr) virt");
-		//my_assert([&] {return my_dynamic_cast<universal_base*>(&*a.data.ptr) != nullptr; },
-		//	"virt pointer must point to type universal_base");
-		universal_base* target = cast_to_universal_base(&*a.data.ptr);
-		data.ptr = uptrcast<stored_t>(target->copy_construct());
+		if (a.data.ptr != nullptr)
+		{
+			any* target = my_dynamic_cast<any*>(&*a.data.ptr);
+			data.ptr = uptrcast<stored_t>(target->copy_construct());
+		}
+		else
+		{
+			data.ptr = nullptr;
+		}
 	}
 
 	virt(virt const& a)
 	{
-		my_assert([&] {return a.data.ptr != nullptr; }, "virt may not be constructed from an empty (nullptr) virt");
-		//my_assert([&] {return my_dynamic_cast<universal_base*>(&*a.data.ptr) != nullptr; },
-		//	"virt pointer must point to type universal_base");
-		universal_base* target = cast_to_universal_base(&*a.data.ptr);
-		data.ptr = uptrcast<stored_t>(target->copy_construct());
+		if(a.data.ptr != nullptr)
+		{
+			any* target = my_dynamic_cast<any*>(&*a.data.ptr);
+			data.ptr = uptrcast<stored_t>(target->copy_construct());
+		}
+		else
+		{
+			data.ptr = nullptr;
+		}
 	}
 
-	template<std::derived_from<stored_t> rhs_t>
+	template<special_derived_from<stored_t> rhs_t>
 	virt& operator=(virt<rhs_t>&& a)
 	{
-		my_assert([&] {return a.data.ptr != nullptr; }, "virt may not be asigned an empty (nullptr) virt");
-		data.ptr = virt{ std::move(a) }.data.ptr;
+		data.ptr = std::move(virt{ std::move(a) }.data.ptr);
 		return *this;
 	}
 
 	virt& operator=(virt&& a)
 	{
-		my_assert([&] {return a.data.ptr != nullptr; }, "virt may not be asigned an empty (nullptr) virt");
-		data.ptr = virt{ std::move(a) }.data.ptr;
+		data.ptr = std::move(virt{ std::move(a) }.data.ptr);
 		return *this;
 	}
 
-	template<std::derived_from<stored_t> rhs_t>
+	template<special_derived_from<stored_t> rhs_t>
 	virt& operator=(virt<rhs_t> const& a)
 	{
-		my_assert([&] {return a.data.ptr != nullptr; }, "virt may not be asigned an empty (nullptr) virt");
-		data.ptr = virt{ a }.data.ptr;
+		data.ptr = std::move(virt{ a }.data.ptr);
 		return *this;
 	}
 
 	virt& operator=(virt const& a)
 	{
-		my_assert([&] {return a.data.ptr != nullptr; }, "virt may not be asigned an empty (nullptr) virt");
-		data.ptr = virt{ a }.data.ptr;
+		data.ptr = std::move(virt{ a }.data.ptr);
 		return *this;
 	}
 
@@ -364,6 +394,7 @@ private:
 	raw_t* get_ptr() const
 	{
 		return my_static_cast<raw_t*>(&*data.ptr);
+
 	}
 public:
 
@@ -381,10 +412,12 @@ public:
 	{
 		return *get_ptr();
 	}
+
 	raw_t& operator*()
 	{
 		return *get_ptr();
 	}
+
 	raw_t const* operator->() const
 	{
 		return get_ptr();
@@ -404,14 +437,18 @@ public:
 		return data.ptr != nullptr;
 	}
 
-	template<std::derived_from<raw_t> tar_t>
+	template<special_derived_from<stored_t> tar_t>
 	bool can_downcast()
 	{
 		my_assert([&] {return bool{ data.ptr }; }, "cannot check can_downcast for a nullptr");
 		return my_dynamic_cast<tar_t*>(&*data.ptr) != nullptr;
 	}
 
-	template<std::derived_from<raw_t> tar_t>
+
+	template<special_derived_from<stored_t> tar_t>
+	bool can_downcast_exact(); //uses some kind of (hand coded) type_id* to check the exact held type
+
+	template<special_derived_from<stored_t> tar_t>
 	tar_t* downcast()
 	{
 		my_assert([&] {return bool{ data.ptr }; }, "cannot downcast a nullptr");
@@ -438,3 +475,5 @@ virt<result_t> make_virt(ts&&...vals)
 	ret.data = virt_data<t>{std::make_unique<derived_wrapper<t>>(std::forward<ts>(vals)... )};
 	return std::move(ret);
 }
+
+
